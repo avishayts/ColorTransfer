fb8228fe40a71688659455063153474a19f51b70
Avishay <avishaytsur1@gmail.com>
2022-03-15 12:17:55 +0200

    update

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 import cv2
 import numpy as np
-from skimage import io, color
 
 refPt = []
 cropping = False
 
 
 def main():
-    pathSource = "photos/1.png"
-    pathTarget = "photos/2.png"
-    source = cv2.imread(pathSource)
-    target = cv2.imread(pathTarget)
-    source = resize(source)
-    clone = source.copy()
-    roi = crop_picture(clone, source)
-    transfer = color_transfer(roi, target)
-    (h, w, _) = roi.shape
-    transfer = cv2.resize(transfer, (w, h), interpolation=cv2.INTER_AREA)
-    source[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]] = transfer
+    pathImage = "photos/forest.jpg"
+    pathTarget = "photos/t.jpg"
+    image = cv2.imread(pathImage)
+    source = cv2.imread(pathTarget)
+    image = resize(image, 800)
+    source = resize(source, 400)
     cv2.imshow("Source", source)
-    show_image("Target", target)
-    # cv2.imshow("Roi", roi)
-    cv2.imshow("Transfer", transfer)
+    clone = image.copy()
+    target = crop_picture(clone, image)
+    transfer = color_transfer(source, target)
+    (h, w, _) = target.shape
+    transfer = cv2.resize(transfer, (w, h), interpolation=cv2.INTER_AREA)
+    image[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]] = transfer
+    cv2.imshow("Image", image)
     cv2.waitKey(0)
 
 
-def resize(image):
-    r = 800 / float(image.shape[1])
-    dim = (800, int(image.shape[0] * r))
-    return cv2.resize(image, (600, 600), interpolation=cv2.INTER_AREA)
+def resize(image, width):
+    height = int(image.shape[0] * (width / float(image.shape[1])))
+    return cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
 
 
 def crop_picture(clone, source):
@@ -62,6 +59,11 @@ def click_and_crop(event, x, y, flags, param):
         # record the ending (x, y) coordinates and indicate that
         # the cropping operation is finished
         refPt.append((x, y))
+
+        # FIX REFPT!!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        # FIX REFPT!!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        # FIX REFPT!!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
         cropping = False
         # draw a rectangle around the region of interest
         cv2.rectangle(param, refPt[0], refPt[1], (0, 255, 0), 2)
@@ -69,122 +71,53 @@ def click_and_crop(event, x, y, flags, param):
 
 
 def image_stats(image):
-    # compute the mean and standard deviation of each channel
+    # split stats and get mean and standard of each stat
     (l, a, b) = cv2.split(image)
     (lMean, lStd) = (l.mean(), l.std())
     (aMean, aStd) = (a.mean(), a.std())
     (bMean, bStd) = (b.mean(), b.std())
-    # return the color statistics
+
+    # return image stats
     return lMean, lStd, aMean, aStd, bMean, bStd
 
 
-def _min_max_scale(arr, new_range=(0, 255)):
-    # get array's current min and max
-    mn = arr.min()
-    mx = arr.max()
-    # check if scaling needs to be done to be in new_range
-    if mn < new_range[0] or mx > new_range[1]:
-        # perform min-max scaling
-        scaled = (new_range[1] - new_range[0]) * (arr - mn) / (mx - mn) + new_range[0]
-    else:
-        # return array if already in range
-        scaled = arr
-
-    return scaled
-
-
-def _scale_array(arr, clip=True):
-    if clip:
-        scaled = np.clip(arr, 0, 255)
-    else:
-        scale_range = (max([arr.min(), 0]), min([arr.max(), 255]))
-        scaled = _min_max_scale(arr, new_range=scale_range)
-
-    return scaled
-
-
-def show_image(title, image, width=300):
-    # resize the image to have a constant width, just to
-    # make displaying the images take up less screen real
-    # estate
-    r = width / float(image.shape[1])
-    dim = (width, int(image.shape[0] * r))
-    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-
-    # show the resized image
-    cv2.imshow(title, resized)
-
-
 def color_transfer(source, target):
-    # # if there are two reference points, then crop the region of interest
-    # # from the image and display it
-    #
-    # # convert the images from the RGB to L*ab* color space, being
-    # # sure to utilizing the floating point data type (note: OpenCV
-    # # expects floats to be 32-bit, so use that instead of 64-bit)
+
+    # convert the images from the RGB to LAB color space
     source = cv2.cvtColor(source, cv2.COLOR_BGR2LAB).astype("float32")
     target = cv2.cvtColor(target, cv2.COLOR_BGR2LAB).astype("float32")
-    #
-    # # compute color statistics for the source and target images
-    # (lMeanSrc, lStdSrc, aMeanSrc, aStdSrc, bMeanSrc, bStdSrc) = image_stats(source)
-    # (lMeanTar, lStdTar, aMeanTar, aStdTar, bMeanTar, bStdTar) = image_stats(target)
-    #
-    # # subtract the means from the target image
-    # (l, a, b) = cv2.split(target)
-    # l -= lMeanTar
-    # a -= aMeanTar
-    # b -= bMeanTar
-    #
-    # l = (lStdTar / lStdSrc) * l
-    # a = (aStdTar / aStdSrc) * a
-    # b = (bStdTar / bStdSrc) * b
-    #
-    # # add in the source mean
-    # l += lMeanSrc
-    # a += aMeanSrc
-    # b += bMeanSrc
-    #
-    # # clip/scale the pixel intensities to [0, 255] if they fall
-    # # outside this range
-    # l = _scale_array(l)
-    # a = _scale_array(a)
-    # b = _scale_array(b)
-    #
-    # # merge the channels together and convert back to the RGB color
-    # # space, being sure to utilize the 8-bit unsigned integer data
-    # # type
-    # transfer = cv2.merge([l, a, b])
-    # transfer = cv2.cvtColor(transfer.astype("uint8"), cv2.COLOR_LAB2BGR)
-    # # return the color transferred image
-    # return transfer
-    s = target
-    t = source
 
-    print("Converting picture...")
-    s_mean, s_std = get_mean_and_std(s)
-    t_mean, t_std = get_mean_and_std(t)
+    # get image stats from the source and target images
+    (lMeanSrc, lStdSrc, aMeanSrc, aStdSrc, bMeanSrc, bStdSrc) = image_stats(source)
+    (lMeanTar, lStdTar, aMeanTar, aStdTar, bMeanTar, bStdTar) = image_stats(target)
 
-    height, width, channel = s.shape
-    for i in range(0, height):
-        for j in range(0, width):
-            for k in range(0, channel):
-                x = s[i, j, k]
-                x = ((x - s_mean[k]) * (t_std[k] / s_std[k])) + t_mean[k]
-                # round or + 0.5
-                x = round(x)
-                # boundary check
-                x = 0 if x < 0 else x
-                x = 255 if x > 255 else x
-                s[i, j, k] = x
-    transfer = cv2.cvtColor(s.astype("uint8"), cv2.COLOR_LAB2BGR)
+    # subtract the mean from the data points
+    (l, a, b) = cv2.split(source)
+    l -= lMeanSrc
+    a -= aMeanSrc
+    b -= bMeanSrc
+
+    # scale the data points comprising the synthetic image by factors determined by the respective standard deviations
+    l = (lStdTar / lStdSrc) * l
+    a = (aStdTar / aStdSrc) * a
+    b = (bStdTar / bStdSrc) * b
+
+    # add the averages computed for the photograph
+    l += lMeanTar
+    a += aMeanTar
+    b += bMeanTar
+
+    # fix the pixels range to [0, 255] if they fall outside this range
+    l = np.clip(l, 0, 255)
+    a = np.clip(a, 0, 255)
+    b = np.clip(b, 0, 255)
+
+    # merge the stats and convert back to the RGB color space
+    transfer = cv2.merge([l, a, b])
+    transfer = cv2.cvtColor(transfer.astype("uint8"), cv2.COLOR_LAB2BGR)
+
+    # return the color transferred image
     return transfer
-
-
-def get_mean_and_std(x):
-    x_mean, x_std = cv2.meanStdDev(x)
-    x_mean = np.hstack(np.around(x_mean, 2))
-    x_std = np.hstack(np.around(x_std, 2))
-    return x_mean, x_std
 
 
 if __name__ == '__main__':
